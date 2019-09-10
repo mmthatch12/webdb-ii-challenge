b5e08f3fbf868cda70bac69bc5270b60977877be
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-10 14:59:32 -0600

    about to test it migration and seed data come in

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,9 +1,18 @@
 const express = require('express')
 
+const db = require('../data/db-config')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Sanity check')
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: 'Could not load data'})
+        })
 })
 
 module.exports = router
