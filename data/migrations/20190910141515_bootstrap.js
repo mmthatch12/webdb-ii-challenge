26a6c99d6c5c67b70c54eee3a9a3ec8dab1869a9
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-10 14:41:41 -0600

    worked on first migration

--- data/migrations/20190910141515_bootstrap.js
+++ data/migrations/20190910141515_bootstrap.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments()
+      tbl.string('VIN').unique().notNullable();
+      tbl.string('Make').unique().notNullable();
+      tbl.string('Model').unique().notNullable();
+      tbl.integer('Mileage').notNullable();
+      tbl.string('Transmission type').nullable();
+      tbl.string('Title status').nullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars')
 };
